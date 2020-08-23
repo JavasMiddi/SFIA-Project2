ce0fd99f6fea68ed1050c631a52aabaa35cee30d
javasmiddleton <javasmiddleton@icloud.com>
2020-08-23 01:15:21 +0000

    If statement on docker install

--- scripts/docker-installs.sh
+++ scripts/docker-installs.sh
@@ -1,5 +1,6 @@
-curl https://get.docker.com | sudo bash
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
+if [docker --version == False]; then
+    
+    curl https://get.docker.com | sudo bash
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
