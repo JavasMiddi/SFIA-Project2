eb80fd873327e397cd99d59facac5cf9d4e291f6
= <=>
2020-08-25 19:41:07 +0000

    Service 1 and 4 fix

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -14,7 +14,5 @@ def fortune():
     number = requests.get("http://service3:5002/fortune/number")    
     fortune = requests.post("http://service4:5003/fortune/fortune", json={"colour":colour.text, "number":number.text})    
     fortune1 = Fortune(colour=colour.text, number=number.text, fortune=fortune.text)
-    db.session.add(fortune1)
-    db.session.commit()
-    
+     
     return render_template('fortune.html', title='Fortune', colour=colour.text, number=number.text, fortune=fortune.text)
