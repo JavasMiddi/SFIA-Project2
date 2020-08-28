5cfda7eee14ff4de4947153a2d3c6e0d358388b5
JavasMiddi <jay@emailcom>
2020-08-28 18:57:14 +0000

    Playbook v1

--- scripts/docker-installs.sh
+++ scripts/docker-installs.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 if [docker --version == False]; then
-    
     curl https://get.docker.com | sudo bash
+    sudo usermod -aG docker $(whoami)
+    sudo apt update
+    sudo apt install -y curl jq python3 python3-pip
+    version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
     sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
+pip3 install pytest
--- service1/testing/test_back_end.py
+++ service1/testing/test_back_end.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
 from application import app, db
