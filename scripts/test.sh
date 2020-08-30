54e54a9b866e495a708701131a79132b4533fe0a
Javas Middleton <javasmiddleton@icloud.com>
2020-08-30 15:57:00 +0000

    Test

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@ pwd
 
 cd service1
 sudo pip3 install -r requirements.txt
-pytest --cov application --cov-report term-missing 
+python3 -m pytest --cov application --cov-report term-missing 
 cd ..
 
 cd service2
