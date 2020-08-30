8f1371fc3568e53b24684f47cbcb4641c7bbd2bf
Javas Middleton <javasmiddleton@icloud.com>
2020-08-30 22:44:49 +0000

    nginx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,18 +2,12 @@
 pwd
 
 ssh master << EOF
-scp /home/javasmiddleton12/SFIA-Project2/docker-compose.yaml /home/jenkins/
+git clone https://github.com/omris-elda/SFIA-Project-2.git
 export SECRET_KEY=${SECRET_KEY} 
 export DATABASE_URI=${DATABASE_URI} 
 
 sudo docker rmi -f $(docker images -a -q)
 docker stack deploy --compose-file docker-compose.yaml SFIA2
-
-docker service scale SFIA2_service1=3 
-docker service scale SFIA2_service2=3
-docker service scale SFIA2_service3=3
-docker service scale SFIA2_service4=3
-docker service scale SFIA2_nginx=3
 
 docker stack services SFIA2
 EOF
