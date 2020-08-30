04efa48c1642aa6e0a48f4e36db72af250429872
Javas Middleton <javasmiddleton@icloud.com>
2020-08-30 23:11:20 +0000

    p

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,10 +9,10 @@ export DATABASE_URI=${DATABASE_URI}
 sudo docker rmi -f $(docker images -a -q)
 docker stack deploy --compose-file docker-compose.yaml SFIA2
 
-sudo docker service scale SFIA2_service1=3
-sudo docker service scale SFIA2_service2=3
-sudo docker service scale SFIA2_service3=3
-sudo docker service scale SFIA2_service4=3
-sudo docker service scale SFIA2_nginx=3
+docker service scale SFIA2_service1=3
+docker service scale SFIA2_service2=3
+docker service scale SFIA2_service3=3
+docker service scale SFIA2_service4=3
+docker service scale SFIA2_nginx=3
 docker stack services SFIA2
 EOF
