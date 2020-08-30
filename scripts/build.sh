1d5eb3c3f5f292bbef4f38edf37f4836e2807469
Javas Middleton <javasmiddleton@icloud.com>
2020-08-30 15:23:02 +0000

    up

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,21 +10,6 @@ sudo apt install python3-pip
 
 pip3 install ansible
 
-/home/jenkins/.local/bin/ansible --version
-/home/jenkins/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
-docker --version
-docker-compose --version
+cd ~/SFIA-Project2
+ansible-playbook -i inventory/cfg playbook.yaml
 
-sudo docker images
-
-sudo docker-compose build
-sudo docker images
-sudo docker system prune -f
-sudo docker images
-sudo docker login
-sudo docker-compose push
-
-sudo docker images
-sudo docker login
-
-sudo docker-compose push
