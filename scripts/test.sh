997efff6fe89a9012f3dd948b8828864855d1e85
Javas Middleton <javasmiddleton@icloud.com>
2020-08-30 17:14:10 +0000

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,22 +1,14 @@
-#! /bin/bash
-pwd
-
-docker ps
-docker exec -it sfia2_service1_1 bash
-python3 create.py
-exit
-
-export DATABASE_URI=mysql+pymysql://root:root@localhost:3306/fortune
-export TEST_DB_URI=mysql+pymysql://root:root@localhost:3306/test
+#!/bin/bash
+pip3 install -r ./requirements.txt
 
 cd service1
-python3 -m pytest --cov application --cov-report term-missing 
-cd ..
+/home/jenkins/.local/bin/pytest
 
-cd service2
-python3 -m pytest --cov application --cov-report term-missing 
-cd ..
+cd ../service2
+/home/jenkins/.local/bin/pytest
 
-cd service3
-python3 -m pytest --cov application --cov-report term-missing
-cd ..
+cd ../service3
+/home/jenkins/.local/bin/pytest
+
+cd ../service4
+/home/jenkins/.local/bin/pytest
