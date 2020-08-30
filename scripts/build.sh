13584f890879178ecb4a4c11c2ead80c3ea35390
Javas Middleton <javasmiddleton@icloud.com>
2020-08-30 16:18:38 +0000

    Up

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,5 +11,15 @@ sudo apt install python3-pip
 sudo pip3 install ansible
 
 ansible-playbook -i ./inventory.cfg ./playbook.yaml
+
+sudo apt update
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker $(whoami)
+sudo apt update
+sudo apt install -y curl jq python3 python3-pip
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo docker-compose build
 sudo docker-compose push
