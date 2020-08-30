6d4eb17ada8e09120f17a4bbf627db7fc3969ccc
Javas Middleton <javasmiddleton@icloud.com>
2020-08-30 18:14:54 +0000

    up

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,10 @@ echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 sudo apt install python3-pip
 sudo pip3 install ansible
 ansible-playbook -i ./inventory.cfg ./playbook.yaml
+sudo docker-compose build
+sudo docker images
+sudo docker system prune -f
 
+sudo docker images
+sudo docker login
+sudo docker-compose push
