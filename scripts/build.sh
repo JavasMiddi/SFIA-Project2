deb090c14a16da776eb8fcbcd000206988f57636
Javas Middleton <javasmiddleton@icloud.com>
2020-08-30 15:28:34 +0000

    Up

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 
 sudo apt install python3-pip
 
-pip3 install ansible
+sudo pip3 install ansible
 
 ansible-playbook -i ./ansible/inventory.cfg ./ansible/playbook.yaml
 
