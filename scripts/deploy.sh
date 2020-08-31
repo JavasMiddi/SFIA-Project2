92e2eb192cca4e6614df5f119f4a400368814a6a
Javas Middleton <javasmiddleton@icloud.com>
2020-08-31 00:03:49 +0000

    clone

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 pwd
 
+ssh sfia-2-worker << EOF
+git clone https://github.com/JavasMiddi/SFIA-Project2.git
+EOF
+
+ssh sfia-2-worker << EOF
+git clone https://github.com/JavasMiddi/SFIA-Project2.git
+EOF
+
 ssh master << EOF
 scp /home/javasmiddleton12/SFIA-Project2/docker-compose.yaml /home/jenkins/
 export SECRET_KEY=${SECRET_KEY} 
